c44553dffeac346a7286c933a7b83beb59cf7015
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-08-12 09:17:47 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -226,7 +226,6 @@ def start(update: Update, context: CallbackContext):
             update.effective_user.first_name
             update.effective_message.reply_sticker(
                 ROSEBOT_STICKER,
-                await message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
